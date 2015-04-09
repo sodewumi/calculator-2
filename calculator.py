3867e103b08d776bcca1d1e612fca3a3aefb095a
Hackbright Student <info@hackbrightacademy.com>
2015-04-08 18:06:35 -0700

    Now able to quit the calculate function

--- calculator.py
+++ calculator.py
@@ -8,47 +8,51 @@ from arithmetic import *
 
 def calculate():
 
-    ask = raw_input("> ")
-    expression = ask.split(" ")
-    symbol = expression[0]
-    # checks the length of the expression
-    if len(expression) == 2:
-        opr = int(expression[1])
-        # checks if the expression is a cube or a square
-        if symbol == 'cube':
-            return cube(opr)
+    quit = False
+
+    while not quit:
+        ask = raw_input("> ")
+        expression = ask.split(" ")
+        symbol = expression[0]
+        # checks the length of the expression
+        if len(expression) == 2:
+            opr = int(expression[1])
+            # checks if the expression is a cube or a square
+            if symbol == 'cube':
+                print cube(opr)
+                
+            elif symbol == 'square':
+                print square(opr)
+            else:
+                print "I don't understand"
+
+        elif len(expression) == 3:
+            opr1 = int(expression[1])
+            opr2 = int(expression[2])
+            if symbol == 'pow':
+                print pow(opr1, opr2)
+
+            elif symbol == 'mod':
+                print mod(opr1, opr2)
             
-        elif symbol == 'square':
-            return square(opr)
+            elif symbol == '+':
+                print add(opr1, opr2)
+            elif symbol == '-':
+                print substract(opr1, opr2)
+            elif symbol == '*':
+                print multiply(opr1, opr2)
+            elif symbol == '/':
+                print divide(opr1, opr2)
+            else:
+                print "I don't understand"
+
         else:
-            return "I don't understand"
+            if len(expression) == 1 and expression[0] == "q":
+                quit = True
+                break
+            print "I don't understand"
 
-    elif len(expression) == 3:
-        opr1 = int(expression[1])
-        opr2 = int(expression[2])
-        if symbol == 'pow':
-            return pow(opr1, opr2)
-
-        elif symbol == 'mod':
-            return mod(opr1, opr2)
-        
-        elif symbol == '+':
-            return add(opr1, opr2)
-        elif symbol == '-':
-            return substract(opr1, opr2)
-        elif symbol == '*':
-            return multiply(opr1, opr2)
-        elif symbol == '/':
-            return divide(opr1, opr2)
-        else:
-            return "I don't understand"
-
-    # else:
-        # the array doen't have a length of 2 or 3
-        # return "I don't understand"
-    return expression
-
-print(calculate())
+calculate()
 
 
 
